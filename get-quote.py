205d246602d8101cbc68b9674612bb023031a8b3
ceydricjb <ceydricjeanbolo@gmail.com>
2020-05-05 17:24:12 +0200

    Randm quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,14 @@
+import random
 def quotescript():
-  print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+  f = open("quotes.txt")
+  quotes = f.readlines()
+  f.close()
 
-  #print(quotes)
+  #last = 13
+  last = len(quotes) - 1 #Note: If you want to add or remove quotes from your text file, you could change the last variable to update automatically
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 
 if __name__== "__main__":
   quotescript()
